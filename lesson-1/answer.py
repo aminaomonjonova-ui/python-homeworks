2fe514601745fda76f25c000f852a609d7ea01e7
aminaomonjonova-ui <aminaomonjonova@gmail.com>
2025-10-28 11:55:55 +0500

    lesson 1 answer

---
+++ lesson-1/answer.py
@@ -0,0 +1,11 @@
+#1. large companies analyze large volumes of data to make better decisions, businesses do that to make more customers
+#2. в exel мы можем пользоваться только формулами, когда в pytone  мы можем быстрее анализировать и сортировать информацию при помощи различныйх библиотек 
+#3. я бы анализировала такое большое количество информации в pytone в библиотеке, чтобы быстро рассортировать и вывести все ошибки файлов.
+#4. in this cases pythone can be useful for: 
+#   - data analyzing 
+#   - data predictions 
+#   - convertations 
+#   - diagramms 
+#   - visual отображение 
+#5. I could load profit data from different years into a pytones DataFrame, then group the data by year and calculate the total profit for each year.
+#6. Without Python, it would be hard to manage and analyze big data efficiently. Manual work in Excel can be very slow and prone to errors, and many advanced analytical techniques like automation, visualization would be difficult to perform.
